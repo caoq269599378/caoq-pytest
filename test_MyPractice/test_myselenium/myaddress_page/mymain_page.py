33bbe39b29976d53b3401d661d12dfc45e35e491
caoq269599378 <269599378@qq.com>
2021-03-05 20:56:28 +0800

    课后作业内容

---
+++ test_MyPractice/test_myselenium/myaddress_page/mymain_page.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from test_MyPractice.test_myselenium.myaddress_page import AddressPage
+class MainPage:
+    def __init__(self):
+        chrome_arg = webdriver.ChromeOptions()
+        chrome_arg.debugger_address = '127.0.0.1:9222'
+        self.driver = webdriver.Chrome(options=chrome_arg)
+        self.driver.implicitly_wait(5)
+
+    def goto_address(self):
+        self.driver.find_element(By.XPATH,"//*[@id = 'menu_contacts']").click()
+        return AddressPage(self.driver)
